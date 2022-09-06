8fbc22c16672ba589b7a63369c1480a2e52d5b94
Tiago Villa <tiago_villa@hotmail.com>
2022-09-06 13:23:59 -0300

    Add Bootcamp model
    + Add data folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
 const connectDB = require('./config/db')
 
 // Load env vars
@@ -26,12 +27,14 @@ const PORT = process.env.PORT || 5000
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
 
 // handle unhandled promises rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`)
+  console.log(`Error: ${err.message}`.red)
   // close server & exit process
   server.close(() => process.exit(1))
 })
